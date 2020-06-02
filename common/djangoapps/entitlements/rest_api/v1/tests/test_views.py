948d8de213b8aa11ab09f452d36fa3b4f2e0687f
Albert (AJ) St. Aubin <astaubin@edx.org>
2020-06-02 10:08:00 -0400

    quality

--- common/djangoapps/entitlements/rest_api/v1/tests/test_views.py
+++ common/djangoapps/entitlements/rest_api/v1/tests/test_views.py
@@ -1051,6 +1051,8 @@ class EntitlementEnrollmentViewSetTest(ModuleStoreTestCase):
     def test_user_can_revoke_and_no_refund_available(
             self,
             mock_get_course_runs,
+            mock_refund_entitlement,
+            mock_is_refundable
     ):
         course_entitlement = CourseEntitlementFactory.create(user=self.user, mode=CourseMode.VERIFIED)
         mock_get_course_runs.return_value = self.return_values
@@ -1093,6 +1095,8 @@ class EntitlementEnrollmentViewSetTest(ModuleStoreTestCase):
     def test_user_is_not_unenrolled_on_failed_refund(
             self,
             mock_get_course_runs,
+            mock_refund_entitlement,
+            mock_is_refundable
     ):
         course_entitlement = CourseEntitlementFactory.create(user=self.user, mode=CourseMode.VERIFIED)
         mock_get_course_runs.return_value = self.return_values
